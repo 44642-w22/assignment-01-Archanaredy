f1b0d776abefafb79d8899b09d6380c9cff33f6f
Archana Reddy Lingampally <89598132+Archanaredy@users.noreply.github.com>
2022-02-08 00:36:21 -0600

    completed assignment01

---
+++ Lingampally_assignment01/src/assignment01/Problem_06.java
@@ -0,0 +1,41 @@
+package assignment01;
+import java.util.*;
+public class Problem_06 {
+public static void main(String args[]) 
+	
+	{
+		Stack<Integer> a = new Stack<Integer>();
+		Scanner ar = new Scanner(System.in);
+		System.out.println("Enter no of integers");
+		int size = ar.nextInt();
+	
+		
+		for(int i=0;i<size;i++)
+		{
+			System.out.println("Enter the element at index "+i);
+			a.add(i, ar.nextInt());
+			
+		}
+		System.out.println("Output(A): " +prob6(a));
+		
+}
+	
+	public static ArrayList<Integer> prob6(Stack<Integer> a)
+	{
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		int size= a.size();
+		 
+	for (int i=a.size()-1;i>=a.size()/2;i--) {
+			
+			 b.add(a.get(i));
+		} 
+		for (int i=0;i<a.size()/2;i++) {
+		
+			b.add(a.get(i));			
+		
+		}
+		 		
+		return b;
+	}
+
+}
